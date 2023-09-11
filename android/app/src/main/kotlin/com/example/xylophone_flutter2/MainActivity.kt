43f4364ffe6be831e304c0240b9f4cb99a94c6b5
AMAN MISHRA <85150925+amanmishra7ave@users.noreply.github.com>
2023-09-11 20:12:23 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/xylophone_flutter2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.xylophone_flutter2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(
+      XylophoneApp(),
+    );
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    void note(int n) {
+      final player = AudioPlayer();
+      player.play(AssetSource('note$n.wav'));
+    }
+
+    Expanded KeyButton(int p) {
+      Color color = Colors.red;
+      switch (p) {
+        case 1:
+          color = Colors.blue;
+          break;
+        case 2:
+          color = Colors.pink;
+          break;
+        case 3:
+          color = Colors.amber;
+          break;
+        case 3:
+          color = Colors.purple;
+          break;
+        case 4:
+          color = Colors.orange;
+          break;
+        case 5:
+          color = Colors.green;
+          break;
+        case 6:
+          color = Colors.cyan;
+          break;
+        case 7:
+          color = Colors.indigo;
+          break;
+      }
+
+      return Expanded(
+        child: Container(
+          color: color,
+          child: TextButton(
+            onPressed: () {
+              note(p);
+            },
+            child: Container(),
+          ),
+        ),
+      );
+    }
+
+    return MaterialApp(
+      // theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                KeyButton(1),
+                KeyButton(2),
+                KeyButton(3),
+                KeyButton(4),
+                KeyButton(5),
+                KeyButton(6),
+                KeyButton(7),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// }
+
+// import 'package:flutter/material.dart';
+// import 'package:audioplayers/audioplayers.dart';
+
+// void main() => runApp(
+//       XylophoneApp(),
+//     );
+
+// class XylophoneApp extends StatelessWidget {
+//   const XylophoneApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     void note(int n) {
+//       final player = AudioPlayer();
+//       player.play(AssetSource(('note$n.wav')));
+//     }
+
+//     Expanded KeyButton(Color color, int soundNumber) {
+//       return Expanded(
+//         child: Container(
+//           color: color,
+//           child: InkWell(
+//             onTap: () {
+//               note(soundNumber);
+//             },
+//             child: Container(),
+//           ),
+//         ),
+//       );
+//     }
+
+//     return MaterialApp(
+//       theme: ThemeData.dark(),
+//       debugBanner: false,
+//       home: Scaffold(
+//         body: Center(
+//           child: SafeArea(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 KeyButton(color: Colors.red, soundNumber: 1),
+//                 KeyButton(color: Colors.red, soundNumber: 2),
+//                 KeyButton(color: Colors.red, soundNumber: 3),
+//                 KeyButton(color: Colors.red, soundNumber: 4),
+//                 KeyButton(color: Colors.red, soundNumber: 5),
+//                 KeyButton(color: Colors.red, soundNumber: 6),
+//                 KeyButton(color: Colors.red, soundNumber: 7),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
